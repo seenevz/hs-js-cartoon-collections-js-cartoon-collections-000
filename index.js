684209f92545f2d338ba764c6ac8cadef089fc88
Sergio Neves <serneves@hotmail.com>
2018-11-27 09:30:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 function dwarfRollCall(dwarves) {
+  var stringArray = []
+
+  for(let i = 0; i < dwarves.length; i++) {
+    stringArray.push(`${i + 1}. ${dwarves[i]}`)
+  }
+  return stringArray.join(` `)
 }
 
 function summonCaptainPlanet(planeteerCalls){
 }
 
 function longPlaneteerCalls(words) {
+
+  if (words > 3) {
+    return true
+  }
+  else {
+    return false
+  }
 }
 
 function findTheCheese (foods) {
